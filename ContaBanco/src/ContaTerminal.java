5464a63b56981aa2f51f1de28a6c7d31d9196795
William Dores <williamdores.s132@outlook.com>
2023-01-04 18:43:59 -0300

    entrega projeto ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //IMPORTANDO A CLASSE ESCANNER
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        
+        // DECLARAÇÃO DAS VARIAVEIS E EXIBIÇÃO DAS MENSAGENS
+        System.out.print("Digite seu nome por gentileza: ");
+        String nomeCliente = sc.nextLine();
+        System.out.print("Por favor, digite o numero da conta: ");
+        int numeroConta = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Digite agora o numero da agencia: ");
+        String numeroAgencia = sc.nextLine();
+        System.out.print("Agora digite o valor a inserir na conta: ");
+        double saldo = sc.nextDouble();
+
+        //MENSAGENS PARA O USUARIO
+        System.out.println("Olá "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+numeroAgencia+", conta "+numeroConta+" e seu saldo " + String.format("%.2f", saldo)+ " já está disponivel para saque.");
+
     }
 }
